3b8d34094b827f7fcceb611b7ce1e2847c33327a
Shinkanchouu <i10566@nara.kosen-ac.jp>
2021-07-02 01:58:22 +0900

    MigrantBirdTestのバグを修正

--- src/MigrantBirdTest.java
+++ src/MigrantBirdTest.java
@@ -10,33 +10,9 @@ public class MigrantBirdTest {
 
     @Test
     public void 眠る() {
-        MigrantBiimport org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
-
-        public class MigrantBirdTest {
-            @Test
-            public void 初期値() {
-                MigrantBird migrantBird = new MigrantBird(9);
-                assertEquals("pos:0 vit:9/9", migrantBird.toString());
-            }
-
-            @Test
-            public void 眠る() {
-                MigrantBird migrantBird = new MigrantBird(12);
-                migrantBird.sleep();
-                assertEquals("pos:3 vit:0/12", migrantBird.toString());
-            }
-
-            @Test
-            public void 鳥の継承確認() {
-                Bird bird = new MigrantBird(13);
-                assertEquals("pos:0 vit:13/13", bird.toString());
-            }
-
-        }
-        rd migrantBird = new MigrantBird(12);
+        MigrantBird migrantBird = new MigrantBird(12);
         migrantBird.sleep();
-        assertEquals("pos:3 vit:0/12", migrantBird.toString());
+        assertEquals("pos:3 vit:12/12", migrantBird.toString());
     }
 
     @Test
@@ -44,5 +20,4 @@ import static org.junit.jupiter.api.Assertions.*;
         Bird bird = new MigrantBird(13);
         assertEquals("pos:0 vit:13/13", bird.toString());
     }
-
 }
